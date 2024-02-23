6154826e30e0af02dc24fce0c7cd28c04ef5aa25
Jaden <jaden.cypes@mymail.champlain.edu>
2024-02-22 20:57:37 -0500

    Done Done Done

---
+++ week4/ParseApacheLogs.ps1
@@ -0,0 +1,22 @@
+﻿clear # clear the screen
+
+function ApacheLogs1() {
+
+$logsNotformatted = Get-Content -Path C:\xampp\apache\logs\access.log
+$tableRecords = @()
+
+for ($i=0; $i -lt $logsNotformatted.Count; $i++){
+# split a string into words
+$words = $logsNotformatted[$i].Split(" ");
+
+$tableRecords += [pscustomobject]@{ "IP" = $words[0]; `
+                                    "Time" = $words[3..4].Trim('['); `
+                                    "Method" = $words[5].Trim('"'); `
+                                    "Page" = $words[6]; `
+                                    "Protocol" = $words[7]; `
+                                    "Response" = $words[8]; `
+                                    "Referrer" = $words[10]; `
+                                    "Client" = $words[11..($words.Count-1)]; }
+    } # end of loop
+return $tableRecords | Where-Object { $_.IP -ilike "10.*" }
+}
--- week4/main.ps1
+++ week4/main.ps1
@@ -1,6 +1,10 @@
 ﻿. (Join-Path $PSScriptRoot Apache-Logs.ps1)
+. (Join-Path $PSScriptRoot ParseApacheLogs.ps1)
 
 clear
 
 $filteredIps = IPfilter index.html 200 Chrome
 $filteredIps
+
+$tableRecords = ApacheLogs1
+$tableRecords | Format-Table -AutoSize -Wrap
