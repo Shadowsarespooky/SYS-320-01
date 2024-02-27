7e8244875e1fdfa313b826db78a4f2ef21ec6295
Jaden <jaden.cypes@mymail.champlain.edu>
2024-02-26 19:18:16 -0500

    All done

--- week5/ScrapingClasses.ps1
+++ week5/ScrapingClasses.ps1
@@ -1,3 +1,60 @@
-﻿function gatherClasses(){
-$page = Invoke-WebRequest -TimeoutSec 2 http:
+﻿clear
+
+function gatherClasses(){
+$page = Invoke-WebRequest -TimeoutSec 2 http://10.0.17.21/Courses.html
+#Downloading the data, may take some time
+
+#Get all the tr elements of HTML document
+$trs=$page.ParsedHTML.body.getElementsByTagName("tr")
+
+# Empty Array to hold elements
+$FullTable = @()
+for($i = 1; $i -lt $trs.length; $i++) {
+    # Get every td element of current tr element
+    $tds = $trs[$i].getElementsByTagName("td")
+   # Write-Host $tds
+    # Want to seperate start time and end time from one time field
+    $Times = $tds[5].innerText.split("-")
+   # Write-Host $Times
+    $FullTable += [pscustomobject]@{"Class Code" = $tds[0].innerText; `
+                                    "Title"      = $tds[1].innerText; `
+                                    "Days"       = $tds[4].innerText; `
+                                    "Time Start" = $Times[0]; `
+                                    "Time End"   = $Times[1]; `
+                                    "Instructor" = $tds[6].innerText; `
+                                    "Location"   = $tds[9].innerText; `
+                                    }
+}# end of for loop
+
+return $FullTable
+}
+
+function daysTranslator($FullTable) {
+#Go over every record in the Table
+for($i=0; $i -lt $FullTable.length; $i++) {
+    #Empty array to hold days for every record
+    $Days = @()
+
+    # If you see "M" -> Monday
+    if($FullTable[$i].Days -ilike "M*"){ $Days += "Monday"}
+
+    # If you see "T" followed by T,W, or F -> Tuesday
+    if($FullTable[$i].Days -ilike "*T[TWF]*"){ $Days += "Tuesday"}
+    
+    # If you see "W" -> Wednesday
+    if($FullTable[$i].Days -ilike "*W*"){ $Days += "Wednesday"}
+    
+    # If you see "TH" -> Thursday
+    if($FullTable[$i].Days -ilike "*TH*"){ $Days += "Thursday"}
+    
+    # If you see "F" -> Friday
+    if($FullTable[$i].Days -ilike "*F"){ $Days += "Friday"}
+
+    # If you see "TBA" -> TBA
+    if($FullTable[$i].Days -ilike "TBA"){ $Days += "TBA"}
+    
+    # Make the switch
+    $FullTable[$i].Days = $Days
+}
+return $FullTable
 }
---
+++ week5/main.ps1
@@ -0,0 +1,33 @@
+﻿. (Join-Path $PSScriptRoot ScrapingClasses.ps1)
+
+#cd $PSScriptRoot
+#..\ScrapingClasses.ps1
+
+$fullTable = gatherClasses
+$translated = daysTranslator $fullTable
+
+# List all the classes of Instructor Furkan Paligu
+#$translated | Select-Object "Class Code", Instructor, Location, Days, "Time Start", "Time End" `
+#                | Where-Object { $_."Instructor" -ilike "Furkan Paligu" }
+
+# List all the classes of JOYC 310 on Mondays, only display the Class Code and Times
+#Sort by Start Time
+#$translated | Where-Object { ($_.Location -ilike "JOYC 310") -and ($_.days -contains "Monday") } | `
+#                Sort-Object "Time Start" | `
+#                Select-Object "Time Start", "Time End", "Class Code"
+
+# Make a list of all the instructors that teach at least 1 course in
+# SYS, SEC, NET, FOR, CSI, DAT
+# Sort by name, and make it unique
+$ITSInstructors = $translated | Where-Object { ($_."Class Code" -ilike "SYS*") -or `
+                                               ($_."Class Code" -ilike "NET*") -or `
+                                               ($_."Class Code" -ilike "SEC*") -or `
+                                               ($_."Class Code" -ilike "FOR*") -or `
+                                               ($_."Class Code" -ilike "CSI*") -or `
+                                               ($_."Class Code" -ilike "DAT*") } `
+                              | Select-Object "Instructor" `
+                              | Sort-Object "Instructor" -Unique
+
+# Group all the instructors together by the number of the classes they are teaching
+$translated | Where-Object { $_.Instructor -in $ITSInstructors.Instructor } `
+            | Group-Object "Instructor" | Select-Object Count, Name | Sort-Object Count -Descending
