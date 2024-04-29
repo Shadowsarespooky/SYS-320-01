cdc1b4899a272fb246fadeee21af3be403a6007e
Jaden <jaden.cypes@mymail.champlain.edu>
2024-04-29 01:19:59 -0400

    Working on it

--- final/htmlReport.bash
+++ final/htmlReport.bash
@@ -2,8 +2,28 @@
 # Challenge-3
 # Write a script that takes the report.txt
 # and transform it into an html report
-# it should be in the /var/ww/html/ dir
-
-
 # have loops to write the html tags in the correct syntax
+touch report.html
+cat << EOF > report
+<!DOCTYPE html>
+<html>
+<body>
+<br>Access logs with IOC indicators: <br>
+<table>
+EOF
 
+while read line; do
+	echo "<tr>" # >> report.html
+	for item in $line; do
+		echo "<td>$item</td>" #>> report.html
+	done
+	echo "</tr>" #>> report.html
+done
+cat << EOF >> report.html
+</table>
+</body>
+</html>
+EOF
+
+# it should be in the /var/ww/html/ dir
+mv report.html /var/www/html/report.html
