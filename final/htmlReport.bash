7c991a1173460bca0f02e157abf254b9bbe8bd03
Jaden <jaden.cypes@mymail.champlain.edu>
2024-04-29 12:42:20 -0400

    C# done!

--- final/htmlReport.bash
+++ final/htmlReport.bash
@@ -4,21 +4,35 @@
 # and transform it into an html report
 # have loops to write the html tags in the correct syntax
 touch report.html
-cat << EOF > report
+cat << EOF > report.html
 <!DOCTYPE html>
 <html>
+<head>
+<style>
+table {
+border-collapse: collapse;
+width: 100%;
+}
+th, td {
+border: 1px solid black;
+padding: 8px;
+text-align: left;
+}
+</style>
+</head>
 <body>
-<br>Access logs with IOC indicators: <br>
+<br>Access logs with IOC indicators: <br> <br>
 <table>
 EOF
 
-while read line; do
-	echo "<tr>" # >> report.html
+while read -r line; do
+	echo "<tr>" >> report.html
 	for item in $line; do
-		echo "<td>$item</td>" #>> report.html
+		echo "<td>$item</td>" >> report.html
 	done
-	echo "</tr>" #>> report.html
-done
+	echo "</tr>" >> report.html
+done < report.txt
+
 cat << EOF >> report.html
 </table>
 </body>
